0cfcccd1c7d0ed9aa634d06919ef9758828108d8
Cristiano Casado <ccasado@corp.globo.com>
2018-10-17 00:14:37 -0300

    interfaces

---
+++ interfaces/model/ave.go
@@ -0,0 +1,28 @@
+package model
+
+//Polimorfismo
+
+//Galinha representa uma ave do tipo Galinha
+type Galinha interface {
+	Cacareja() string
+}
+
+//Pata representa uma ave do tipo Pata
+type Pata interface {
+	Quack() string
+}
+
+//Ave representa um animal
+type Ave struct {
+	Nome string
+}
+
+//Cacareja retorna o som emitido por uma galinha
+func (a Ave) Cacareja() string {
+	return "Cócóricó..."
+}
+
+//Quack retorna o som emitido por uma pata
+func (a Ave) Quack() string {
+	return "Quack..."
+}
--- slices/main.go
+++ slices/main.go
@@ -14,16 +14,31 @@ func main() {
 	fmt.Println(capitais, len(capitais), cap(capitais))
 	capitais = append(capitais, "Brasilia")
 	fmt.Println(capitais, len(capitais), cap(capitais))
-	cidades := make([]string, 4)
-	cidades[0] = "NY"
-	cidades[1] = "LO"
-	cidades[2] = "TO"
-	cidades[3] = "SG"
+	cidades := make([]string, 5)
+	cidades[0] = "Nova York"
+	cidades[1] = "Londres"
+	cidades[2] = "Madeira"
+	cidades[3] = "Tokio"
+	cidades[4] = "Singapura"
 	fmt.Println(cidades, len(cidades), cap(cidades))
-	cidades[0] = "BR"
+	cidades[0] = "NY"
 	fmt.Println(cidades, len(cidades), cap(cidades))
 	for indice, cidade := range cidades {
 		fmt.Printf("Cidade[%d] = %s\r\n", indice, cidade)
 	}
+	// Primeiro item começa com indice 0
+	// Segundo item começa com indice 1
+	capitaisAsia := cidades[3:5]
+	fmt.Println(capitaisAsia, len(capitaisAsia), cap(capitaisAsia))
+	temp1 := cidades[:2]
+	fmt.Println(temp1, len(temp1), cap(temp1))
+	temp2 := cidades[len(cidades)-2:]
+	fmt.Println(temp2, len(temp2), cap(temp2))
+	// Remover item do slice
+	indiceDoItemARetirar := 2
+	temp := cidades[:indiceDoItemARetirar]
+	temp = append(temp, cidades[indiceDoItemARetirar+1:]...)
+	copy(cidades, temp)
+	fmt.Println(cidades)
 
 }
